40e87094ad96c5c3d2024045d3f61f12f99549cc
Raphael <rt_phaelniggaz@live.com>
2026-01-12 12:38:46 -0300

    Config axios, implement new pages and styles

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import blogFetch from "../axios/config";
+
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [post, setPost] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const response = await blogFetch.get("/posts");
+      const data = response.data;
+      setPost(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <div className="home">
+      <h1>Últimos posts</h1>
+      {post.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        post.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/post/${post.id}`} className="btn">
+              Ler mais...
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Home;
